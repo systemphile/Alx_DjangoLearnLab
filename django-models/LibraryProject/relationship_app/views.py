562a04919725452f5867e8129f7582eff1592fc1
systemphile <systemphile@gmail.com>
2025-07-20 18:33:46 +0300

    Implementing User Authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from .models import Library, Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.    
 def list_books(request):
