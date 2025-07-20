17b76eaa94334f875d0d428bd8ca6917b00a2410
systemphile <systemphile@gmail.com>
2025-07-20 19:33:50 +0300

    Implementing Custom Permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, login_required, user_passes_test
 from .forms import BookForm
 
 # Create your views here.  
